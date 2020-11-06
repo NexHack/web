9d0c20c5789e2ebf2431441e915c27725d021566
Jayesh Mann <jayeshmann06@gmail.com>
2020-11-06 23:54:35 +0530

    Added services & action creators v20201106.5a for login and logout states as well as auto-login

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,3 +1,45 @@
+import loginService from '../services/login'
+import userService from '../services/user'
+
+export const login = (username, password) => async dispatch => {
+  try {
+    const loggedInUser = await loginService.login({ username, password })
+
+    window.localStorage.setItem('loggedAppUser', JSON.stringify(loggedInUser))
+
+    userService.setToken(loggedInUser.token)
+    dispatch({
+      type: 'LOGIN',
+      data: loggedInUser,
+    })
+    // dispatch(notify(`Welcome ${loggedInUser.name}`))
+  } catch (error) {
+    if (JSON.stringify(error).includes('401')) {
+      // dispatch(notify('Wrong Credentials', 'error'))
+    } else {
+      // dispatch(notify('Internet/Server Issue', 'error'))
+    }
+  }
+}
+
+export const logout = () => async dispatch => {
+  window.localStorage.removeItem('loggedAppUser')
+  dispatch({ type: 'LOGOUT' })
+}
+
+export const check = () => async dispatch => {
+  const loggedUserJSON = window.localStorage.getItem('loggedAppUser')
+  if (loggedUserJSON) {
+    const loggedInUser = JSON.parse(loggedUserJSON)
+    userService.setToken(loggedInUser.token)
+    dispatch({
+      type: 'CHECK',
+      data: loggedInUser,
+    })
+    // dispatch(notify(`Welcome ${loggedInUser.name}`))
+  }
+}
+
 const userReducer = (state = null, action) => {
   switch (action.type) {
     case 'LOGIN':
