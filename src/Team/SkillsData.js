47b3a89a3c767984b928a5f4b5673cd706b18dd7
shreya <shreyasingh2284@gmail.com>
2020-11-07 18:43:53 +0530

    update with main

--- src/Team/SkillsData.js
+++
@@ -1,9 +0,0 @@
-export const skills = [
-    { id: 1, name: 'React' },
-    { id: 2, name: 'NodeJs' },
-    { id: 3, name: 'Django' },
-    { id: 4, name: 'MongoDB' },
-    { id: 5, name: 'Firebase' },
-
-  ];
-  
